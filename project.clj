9a70babf0e5f80b056c10f876c6d6df7fc8f2260
Mark Bastian <markbastian@gmail.com>
2020-06-24 23:41:06 -0600

    Docs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-north-2020 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A self-guided workshop presented at Clojure/north 2020"
+  :url "https://github.com/markbastian/clojure-north-2020"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
